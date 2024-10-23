469d7a26c547202583290ee9d40497980af130e9
Mangassouba <hamallahmanga@gmail.com>
2024-10-23 19:29:03 +0000

    add file productRoute

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -1,1 +1,18 @@
+import express from 'express';
+import {
+  createProduct,
+  getAllProducts,
+  getProductById,
+  updateProduct,
+  deleteProduct,
+} from '../controllers/productController.js';
 
+const router = express.Router();
+
+router.post('/product', createProduct);
+router.get('/products', getAllProducts);
+router.get('/product/:id', getProductById);
+router.put('/product/:id', updateProduct);
+router.delete('/product/:id', deleteProduct);
+
+export default productRoute;
