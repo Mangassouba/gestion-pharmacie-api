b2e12b1cf4f5faeedbd037c89a689bdc88627bf7
Mangassouba <hamallahmanga@gmail.com>
2024-10-28 15:57:05 +0000

    add sale access permission

--- src/routes/saleRoutes.js
+++ src/routes/saleRoutes.js
@@ -1,13 +1,14 @@
 import express from "express";
 import { createSale, getSales, updateSale, deleteSale, getSaleById } from "../controllers/saleController.js"
 import { addSaleValidator, getSaleByIdValidator, updateSaleValidator, deleteSaleValidator  } from "../validators/saleValidator.js";
+import { authenticateToken,authorizeRole } from '../middlewares/authMiddleware.js';
 
 const saleRouter = express.Router();
 
-saleRouter.post("/sale",addSaleValidator,  createSale);
-saleRouter.get("/sale", getSales);
-saleRouter.get("/sale/:id",getSaleByIdValidator,  getSaleById);
-saleRouter.put("/sale/:id",updateSaleValidator, updateSale);
-saleRouter.delete("/sale/:id",deleteSaleValidator,  deleteSale);
+saleRouter.post("/sale", authenticateToken, addSaleValidator,  createSale);
+saleRouter.get("/sale", authenticateToken, getSales);
+saleRouter.get("/sale/:id", authenticateToken, getSaleByIdValidator,  getSaleById);
+saleRouter.put("/sale/:id", authenticateToken, updateSaleValidator, updateSale);
+saleRouter.delete("/sale/:id", authenticateToken, authorizeRole, deleteSaleValidator,  deleteSale);
 
 export default saleRouter;
