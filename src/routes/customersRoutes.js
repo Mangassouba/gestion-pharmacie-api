0a0e80354283854b2303387c4e100b47ca419f76
Mangassouba <hamallahmanga@gmail.com>
2024-10-28 15:49:07 +0000

    add customers access permission

--- src/routes/customersRoutes.js
+++ src/routes/customersRoutes.js
@@ -6,14 +6,15 @@ import {
   updateClient,
   deleteClient,
 } from '../controllers/customersController.js';
+import { authenticateToken,authorizeRole } from '../middlewares/authMiddleware.js';
 import { createClientValidator,updateClientValidator,deleteClientValidator,getClientByIdValidator } from '../validators/customersValidator.js';
 
 const clientRouter = express.Router();
 
-clientRouter.post('/customers',createClientValidator, createClient);
-clientRouter.get('/customers', getAllClients);
-clientRouter.get('/customers/:id', getClientByIdValidator, getClientById);
-clientRouter.put('/customers/:id',updateClientValidator, updateClient);
-clientRouter.delete('/customers/:id',deleteClientValidator, deleteClient);
+clientRouter.post('/customers', authenticateToken, createClientValidator, createClient);
+clientRouter.get('/customers', authenticateToken, getAllClients);
+clientRouter.get('/customers/:id', authenticateToken, getClientByIdValidator, getClientById);
+clientRouter.put('/customers/:id', authenticateToken, updateClientValidator, updateClient);
+clientRouter.delete('/customers/:id', authenticateToken, authorizeRole, deleteClientValidator, deleteClient);
 
 export default clientRouter;
