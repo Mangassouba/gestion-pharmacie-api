96f34647e6f0b75d0f470276015c4a98c2d13673
Mangassouba <hamallahmanga@gmail.com>
2024-10-28 16:00:47 +0000

    add supplier access permission

--- src/routes/supplierRoutes.js
+++ src/routes/supplierRoutes.js
@@ -12,13 +12,14 @@ import {
   deleteSupplierValidator,
   getSupplierByIdValidator,
 } from '../validators/supplierValidator.js';
+import { authenticateToken,authorizeRole } from '../middlewares/authMiddleware.js';
 
 const supplierRouter = express.Router();
 
-supplierRouter.post('/suppliers', createSupplierValidator, createSupplier);
-supplierRouter.get('/suppliers', getAllSuppliers);
-supplierRouter.get('/suppliers/:id', getSupplierByIdValidator, getSupplierById);
-supplierRouter.put('/suppliers/:id', updateSupplierValidator, updateSupplier);
-supplierRouter.delete('/suppliers/:id', deleteSupplierValidator, deleteSupplier);
+supplierRouter.post('/suppliers', authenticateToken, createSupplierValidator, createSupplier);
+supplierRouter.get('/suppliers', authenticateToken, getAllSuppliers);
+supplierRouter.get('/suppliers/:id', authenticateToken, getSupplierByIdValidator, getSupplierById);
+supplierRouter.put('/suppliers/:id', authenticateToken, updateSupplierValidator, updateSupplier);
+supplierRouter.delete('/suppliers/:id', authenticateToken, authorizeRole, deleteSupplierValidator, deleteSupplier);
 
 export default supplierRouter;
