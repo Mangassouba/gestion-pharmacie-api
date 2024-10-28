4c2cc3a8a6f546aa784dcbbd7afd926ed23fb362
Mangassouba <hamallahmanga@gmail.com>
2024-10-28 15:53:20 +0000

    add orders access permission

--- src/routes/ordersRoutes.js
+++ src/routes/ordersRoutes.js
@@ -2,14 +2,15 @@
 import express from 'express';
 import { createOrder, getOrders, getOrderById, updateOrder, deleteOrder } from '../controllers/ordersController.js';
 import { createOrderValidator, deleteOrderValidator, updateOrderValidator, getOrderByIdValidator } from '../validators/ordersValidator.js';
+import { authenticateToken,authorizeRole } from '../middlewares/authMiddleware.js';
 
 const ordersRouter = express.Router();
 
 
-ordersRouter.post('/orders',createOrderValidator, createOrder);
-ordersRouter.get('/orders', getOrders);
-ordersRouter.get('/orders',getOrderByIdValidator, getOrderById);
-ordersRouter.put('/orders/:id',updateOrderValidator, updateOrder);
-ordersRouter.delete('/orders/:id',deleteOrderValidator, deleteOrder);
+ordersRouter.post('/orders', authenticateToken, createOrderValidator, createOrder);
+ordersRouter.get('/orders', authenticateToken, getOrders);
+ordersRouter.get('/orders', authenticateToken, getOrderByIdValidator, getOrderById);
+ordersRouter.put('/orders/:id', authenticateToken, updateOrderValidator, updateOrder);
+ordersRouter.delete('/orders/:id', authenticateToken, authorizeRole, deleteOrderValidator, deleteOrder);
 
 export default ordersRouter;
