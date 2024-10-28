c0ca329798843d2d8b89202cd6b281cc0986e744
Mangassouba <hamallahmanga@gmail.com>
2024-10-28 15:46:40 +0000

    add batch access permission

--- src/routes/batchesRoutes.js
+++ src/routes/batchesRoutes.js
@@ -6,14 +6,15 @@ import {
   updateBatch,
   deleteBatch,
 } from "../controllers/batchesController.js";
+import { authenticateToken,authorizeRole } from '../middlewares/authMiddleware.js';
 import { batchIdValidator,batchupdateValidator, batchValidator, deleteBatchValidator } from "../validators/batchesValidator.js";
 
 const batcheRrouter = express.Router();
 
-batcheRrouter.post("/batches",batchValidator,  createBatch);
-batcheRrouter.get("/batches", getBatches);
-batcheRrouter.get("/batches/:id", batchIdValidator, getBatchById);
-batcheRrouter.put("/batches/:id",batchupdateValidator,  updateBatch);
-batcheRrouter.delete("/batches/:id", deleteBatchValidator, deleteBatch);
+batcheRrouter.post("/batches", authenticateToken, batchValidator,  createBatch);
+batcheRrouter.get("/batches", authenticateToken, getBatches);
+batcheRrouter.get("/batches/:id", authenticateToken, batchIdValidator, getBatchById);
+batcheRrouter.put("/batches/:id", authenticateToken, batchupdateValidator,  updateBatch);
+batcheRrouter.delete("/batches/:id", authenticateToken, authorizeRole, deleteBatchValidator, deleteBatch);
 
 export default batcheRrouter;
