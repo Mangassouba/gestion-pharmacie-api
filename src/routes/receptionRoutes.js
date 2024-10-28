66d1e8bc38c85c9dfa96ab5bcd50e2409b9f747d
Mangassouba <hamallahmanga@gmail.com>
2024-10-28 15:54:53 +0000

    add reception access permission

--- src/routes/receptionRoutes.js
+++ src/routes/receptionRoutes.js
@@ -12,13 +12,14 @@ import {
   updateReceptionValidator,
   deleteReceptionValidator
 } from "../validators/receptionValidator.js";
+import { authenticateToken,authorizeRole } from '../middlewares/authMiddleware.js';
 
 const receptionsRouter = express.Router();
 
-receptionsRouter.post("/receptions", createReceptionValidator, createReception);
-receptionsRouter.get("/receptions", getReceptions);
-receptionsRouter.get("/receptions/:id", getReceptionByIdValidator, getReceptionById);
-receptionsRouter.put("/receptions/:id", updateReceptionValidator, updateReception);
-receptionsRouter.delete("/receptions/:id", deleteReceptionValidator, deleteReception);
+receptionsRouter.post("/receptions", authenticateToken, createReceptionValidator, createReception);
+receptionsRouter.get("/receptions", authenticateToken, getReceptions);
+receptionsRouter.get("/receptions/:id", authenticateToken, getReceptionByIdValidator, getReceptionById);
+receptionsRouter.put("/receptions/:id", authenticateToken, updateReceptionValidator, updateReception);
+receptionsRouter.delete("/receptions/:id", authenticateToken, authorizeRole, deleteReceptionValidator, deleteReception);
 
 export default receptionsRouter;
